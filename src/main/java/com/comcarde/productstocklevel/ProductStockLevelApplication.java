80ec6d7d7df3ca2cf69f1da133e9e75b96748fde
Ram Ala <ram.ala@sky.uk>
2020-02-05 22:49:48 +0000

    |ram ala|Initial project set up

---
+++ src/main/java/com/comcarde/productstocklevel/ProductStockLevelApplication.java
@@ -0,0 +1,13 @@
+package com.comcarde.productstocklevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockLevelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockLevelApplication.class, args);
+	}
+
+}
